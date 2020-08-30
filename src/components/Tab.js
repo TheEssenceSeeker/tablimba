c882b6913464219779e9195328c6d45fd59ceff1
TheEssenceSeeker <56074643+TheEssenceSeeker@users.noreply.github.com>
2020-08-30 18:45:19 +0300

    Bar lines added

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,21 +1,45 @@
 import React from 'react'
 import TabRow from "./TabRow"
-import {parseNote} from '../misc/tabHandling'
+import {parseNote, sumDurations} from '../misc/tabHandling'
+import * as Tone from "tone"
+import TabBarLine from "./TabBarLine"
 
 const Tab = props => {
     const {tab, tuning, highlightedNotes, editNote, deleteRow, insertRow} = props
+    let prevNoteBarNumber = 0
+    let currentTime = Tone.Time(0)
+
     return (
         <div className='tab'>
             {
-                tab.map((strNote, i) => <TabRow note={parseNote(strNote)}
-                                                tuning={tuning}
-                                                highlightedNotes={highlightedNotes}
-                                                editNote={editNote}
-                                                index={i}
-                                                deleteRow={deleteRow}
-                                                insertRow={insertRow}
-                                                key={i}
-                />)
+                tab.map((strNote, i) => {
+                    let hasBarLine = false
+                    const note = parseNote(strNote)
+                    currentTime = sumDurations(currentTime.toBarsBeatsSixteenths(), note.duration)
+                    const barNumber = +currentTime.toBarsBeatsSixteenths().split(':')[0]
+                    console.log(barNumber, prevNoteBarNumber)
+                    if (barNumber !== prevNoteBarNumber) {
+                        hasBarLine = true
+                        prevNoteBarNumber = barNumber
+                    }
+
+                    return (
+                        <>
+                            <TabRow note={note}
+                                    tuning={tuning}
+                                    highlightedNotes={highlightedNotes}
+                                    editNote={editNote}
+                                    index={i}
+                                    deleteRow={deleteRow}
+                                    insertRow={insertRow}
+                                    key={i}
+                            />
+                            {
+                                hasBarLine && <TabBarLine number={barNumber}/>
+                            }
+                        </>
+                    )
+                })
             }
         </div>
     )
---
+++ src/components/TabBarLine.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TabBarLine = ({number}) => (
+    <div className="kalimba-row bar-line"><div className='bar-number'>{number}</div></div>
+)
+
+export default TabBarLine
