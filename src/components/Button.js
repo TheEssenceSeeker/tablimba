0c769e978245a190c92c82d168705ce79516938a
TheEssenceSeeker <56074643+TheEssenceSeeker@users.noreply.github.com>
2020-09-10 06:06:16 +0300

    Refactored Button with styled-components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Button = ({children, onClick}) => {
-    return (
-        <div className='button' onClick={onClick}>
-            <span className='button__text'>{children}</span>
-        </div>
-    )
-}
+const Button = styled.button`
+  outline: none;
+  border: none;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  height: 25px;
+  padding: 5px;
+  box-shadow: 0 0 0 2px gray;
+  background-color: #fff;
+  border-radius: 3px;
+  font-size: 15px;
+  margin: 2px;
+  cursor: pointer;
+  
+  &:focus {
+    box-shadow: 0 0 0 2px grey,
+                0 0 0 3px black;
+  }
+  
+  &:hover {
+    box-shadow: 0 0 0 2px #000;
+  }
+  
+  &:active {
+    background-color: ${props => props.theme.accent};
+  }
+`
 
 export default Button
