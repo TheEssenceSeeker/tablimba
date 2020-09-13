d49270daa7f744e32f3a3362e53f2bc9b378c5b1
TheEssenceSeeker <56074643+TheEssenceSeeker@users.noreply.github.com>
2020-09-13 17:13:45 +0300

    Finished refactoring

--- src/components/TabNote/index.js
+++ src/components/TabNote/index.js
@@ -1,43 +1,7 @@
 import React from 'react'
 import {noteSymbols, restSymbols} from '../../misc/tabHandling'
-import styled from 'styled-components'
-
-const TabNoteContainer = styled.div`
-  display: inline-flex;
-  flex: 1 0;  
-  border-right: 1px solid #000;
-  border-left: 1px solid #000;
-  background-color: ${p => p.isHighlighted ? p.theme.highlight : 'inherit'};
-  color: #000;
-  justify-content: center;
-  user-select: none;
-  cursor:pointer;
-
-  &::before {
-    content:'';
-    float:left;
-    padding-top: 100%;
-  }
-`
-
-const Note = styled.div`
-  font-size: 35px;
-  @media (max-width: 800px) {
-    font-size: 30px;
-  }
-  @media (max-width: 700px) {
-    font-size: 25px;
-  }
-  @media (max-width: 550px) {
-    font-size: 22px;
-  }
-  @media (max-width: 480px) {
-    font-size: 18px;
-  }
-  @media (max-width: 400px) {
-    font-size: 15px;
-  }
-`
+import TabNoteContainer from "./TabNoteContainer"
+import Note from "./Note"
 
 const TabNote = ({filled = false, isHighlighted, note, editNote, index, pitch, className}) =>
     (
