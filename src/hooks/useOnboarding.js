727f26f82d863ac44104c06447b4783ab61c150c
TheEssenceSeeker <56074643+TheEssenceSeeker@users.noreply.github.com>
2020-09-21 01:18:47 +0300

    Now onboarding only shows once (status saves in localStorage)

--- src/hooks/useOnboarding.js
+++ src/hooks/useOnboarding.js
@@ -1,13 +1,12 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {isMobile} from "react-device-detect"
-import Joyride from "react-joyride"
+import Joyride, { ACTIONS, EVENTS, STATUS } from "react-joyride"
 
 export const useOnboarding = () => {
 
     const [isRunning, setIsRunning] = useState(false)
-
+    const [isFinished, setIsFinished] = useState(true)
     const actionText = isMobile ? 'Long tap' : 'Right click'
-
     const [steps, setSteps] = useState([
         {
             target: '.container-tuning',
@@ -26,11 +25,33 @@ export const useOnboarding = () => {
         },
     ])
 
+    useEffect(() => {
+        setIsFinished(localStorage.getItem('onboardingFinished') === 'true')
+    })
+
     const start = () => setIsRunning(true)
     const stop = () => setIsRunning(false)
 
+    const handleJoyrideCallback = data => {
+        const { status } = data
+
+        if ([STATUS.FINISHED, STATUS.SKIPPED].includes(status)) {
+            setIsRunning(false)
+            localStorage.setItem('onboardingFinished', 'true')
+        }
+    }
+
     return [
-        <Joyride steps={steps} run={isRunning} continuous={true}/>,
+        (
+            !isFinished ?
+                <Joyride
+                    callback={handleJoyrideCallback}
+                    steps={steps}
+                    run={isRunning}
+                    continuous
+                    showSkipButton /> :
+                null
+        ),
         start,
         stop
         ]
