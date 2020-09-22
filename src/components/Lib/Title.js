6a9ba6de32539362b778711890ae4bf4554f40f3
TheEssenceSeeker <56074643+TheEssenceSeeker@users.noreply.github.com>
2020-09-22 06:20:56 +0300

    Reverted Title changes

--- src/components/Lib/Title.js
+++ src/components/Lib/Title.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
 
 const Title = styled.h1`
-  font-size: 1.8rem;
+  font-size: 1.2rem;
   user-select: none;
+  margin-bottom: 0.5rem;
 `
 
 export default Title
