72e7f17cabb91487374f920f0a21d633372ee95f
TheEssenceSeeker <56074643+TheEssenceSeeker@users.noreply.github.com>
2020-09-22 23:37:15 +0300

    Menu now closing on clicking outside

--- src/components/SideMenu/index.js
+++ src/components/SideMenu/index.js
@@ -1,21 +1,30 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import SideMenuContainer from "./SideMenuContainer"
 import Logo from "./Logo"
 import Navbar from "./Navbar"
 import Burger from "./Burger"
+import useOnClickOutside from "../../hooks/useOnClickOutside"
 
 const SideMenu = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const menuRef = useRef(null)
+
+  useOnClickOutside(
+    menuRef,
+    () => setIsOpen(false)
+  )
 
   return (
-    <>
+    <div ref={menuRef}>
       <Burger isToggled={isOpen} onClick={() => setIsOpen(prev => !prev)} />
 
-      <SideMenuContainer isOpen={isOpen}>
+      <SideMenuContainer
+        isOpen={isOpen}
+      >
         <Logo />
         <Navbar onClickLink={() => setIsOpen(false)} />
       </SideMenuContainer>
-    </>
+    </div>
   )
 }
 
